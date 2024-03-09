12c56ef9e2aa9cb62687aa8e9c6ac5631ca341b0
Ally Garton <alexgarton@me.com>
2024-03-09 20:42:36 +0000

    added to readme, corrected comment

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FortuneBot
  A Discord bot to tell your fortune.
+
+## Magic 8 Ball
+A standard Magic 8 Ball. Takes in a ```question``` and returns its prediction.
+
+## Numerology (birthday)
+Input your birthday - ```date```, ```month```, and ```year``` - and FortuneBot will calculate your life path number.
--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def magic_8ball(interaction, question: str):
     await interaction.response.send_message(f'You asked: {question} \n\nMagic 8 Ball says: {responses[number].lower()}')
 
 
-# magic 8-ball command #
+# numerology (birthday) command #
 @tree.command(name="numerology", description="Find your life path number.", guild=discord.Object(id=GUILD_ID))
 async def numbers(interaction, date: int, month: int, year: int):
     if (date > 31 or date < 1) or (month < 1 or month > 12) or (year < 1000 or year > 9999) or (month == (4 or 6 or 9 or 11) and date > 30) or (month == 2 and date > 28 and (year % 4 != 0)) or (month == 2 and date > 29 and (year % 4 == 0)):
