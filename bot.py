7bd0ade93227395476b26172a083d148413118e5
Ally Garton <alexgarton@me.com>
2024-03-09 19:58:24 +0000

    added magic 8 ball functionality

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import json
 from discord import app_commands
 from dotenv import load_dotenv
 
+from _8ball import responses
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -32,6 +34,13 @@ async def on_ready():
 @tree.command(name = "hello", description = "just saying hi", guild=discord.Object(id=GUILD_ID))
 async def first_command(interaction):
     await interaction.response.send_message("oh, hello there!")
+
+
+# magic 8-ball command #
+@tree.command(name="magic8ball", description="Ask anything!", guild=discord.Object(id=GUILD_ID))
+async def magic_8ball(interaction, question: str):
+    number = random.randint(0, len(responses))
+    await interaction.response.send_message(f'You asked: {question} \n\nMagic 8 Ball says: {responses[number].lower()}')
 
 
 
